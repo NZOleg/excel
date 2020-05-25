3a8734f1cce7a4e19018a9f40ebf0161a57abe74
Oleg Nesterov <nzolegok@gmail.com>
2020-05-25 22:12:58 +1200

    finish css layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './scss/index.scss';
+import './scss/header.scss';
+import './scss/toolbar.scss';
+import './scss/formula.scss';
+import './scss/table.scss';
+import './scss/dashboard.scss';
 
 console.log('working');
